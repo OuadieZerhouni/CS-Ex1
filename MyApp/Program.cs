329fc6340910a00c93afcb1911ad9bdb11c3191f
Wadie <ouadie.zerhouni@usmba.ac.ma>
2022-10-25 11:33:00 +0200

    first commit

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -10,7 +10,34 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            
+            bool noprem = true;
+            while(noprem)
+            {
+                Console.WriteLine("Entrer un nombre : ");
+                string inpt = Console.ReadLine();
+                int.TryParse(inpt, out int number); ;
+
+                noprem = !Prem(number);
+                if (!noprem)
+                {
+                    Console.WriteLine("est premier!!");
+                }
+            }
+
+            Console.ReadLine();
+
+        }
+        static bool Prem(int num)
+        {
+            if (num < 1) return true;
+            if (num == 1 | num == 2) return true;
+            if (num % 2 == 0) return false;
+
+            for (int i = 3; i < num; i += 2)
+                if (num % i == 0)
+                    return false;
+
+            return true;
         }
     }
 }
